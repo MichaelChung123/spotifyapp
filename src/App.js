4ccd4fe6f901bd67d1002a9398d375046c61dd79
MichaelChung123 <michael.chung1994@gmail.com>
2019-05-01 16:14:28 -0700

    connected psql to front end

--- src/App.js
+++ src/App.js
@@ -4,40 +4,47 @@ import './App.css';
 
 class App extends Component {
   state = {
-    data: null
+    data: null,
+    users: [],
+    loaded: false
   };
 
   componentDidMount() {
-    // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
+    fetch('/users')
+      .then((response) => { return response.json() })
+      .then((data) => {
+        this.setState({
+          users: data,
+          loaded: true
+        })
+      });
   };
 
   render() {
+    let users = this.state.users.map((item, i) => {
+      return <p>{item.name}</p>
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {/* Render the newly fetched data inside of this.state.data  */}
-        <p className="App-intro">{this.state.data}</p>
+      <div>
+        {
+          this.state.loaded ? (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              {users}
+            </div>
+          ) : (
+              <div>
+                <p>L O A D I N G . . .</p>
+              </div>
+            )
+        }
       </div>
     );
   }
-
 }
 
 export default App;

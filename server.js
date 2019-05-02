2129cb00c469bd8be983a8e00df33033e0736468
MichaelChung123 <michael.chung1994@gmail.com>
2019-05-02 13:42:57 -0700

    connected spotify api

--- server.js
+++ server.js
@@ -1,42 +1,22 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const app = express()
-// const port = 3000
-// const db = require('./queries')
-
-
-// app.use(bodyParser.json())
-// app.use(
-//     bodyParser.urlencoded({
-//         extended: true,
-//     })
-// )
-
-// app.get('/', (request, response) => {
-//     response.json({
-//         info: 'Node.js, Express, and Postgres API'
-//     })
-// })
-
-// app.get('/users', db.getUsers)
-// app.get('/users/:id', db.getUserById)
-// app.post('/users', db.createUser)
-// app.put('/users/:id', db.updateUser)
-// app.delete('/users/:id', db.deleteUser)
-
-// app.listen(port, () => {
-//     console.log(`App running on port ${port}.`)
-// })
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
 const db = require('./queries')
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/', (request, response) => {
+    response.json({
+        info: 'Node.js, Express, and Postgres API'
+    })
+})
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
@@ -44,5 +24,47 @@ app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
+
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 5000;
+// const db = require('./queries');
+// // const SpotifyWebApi = require('spotify-web-api-node');
+
+// // let spotifyApi = new SpotifyWebApi({
+// //   clientId: 'c406ef3bf9c64b7f95f9aa4bcd975af6',
+// //   clientSecret: '1ac1d746426743f392d707ca1815a834',
+// //   redirectUri: 'http://http://localhost:3000/'
+// // });
+
+// // // Get Elvis' albums
+// // spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+// //   function (data) {
+// //     console.log('Artist albums', data.body);
+// //   },
+// //   function (err) {
+// //     console.error(err);
+// //   }
+// // );
+
+
+// // create a GET route
+// app.get('/express_backend', (req, res) => {
+//   res.send({
+//     express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'
+//   });
+// });
+
+// app.get('/users', db.getUsers)
+// app.get('/users/:id', db.getUserById)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
+
+// // console.log that your server is up and running
+// app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,46 +1,78 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  state = {
-    data: null,
-    users: [],
-    loaded: false
-  };
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    // console.log('Params: ', params);
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
 
-  componentDidMount() {
-    fetch('/users')
-      .then((response) => { return response.json() })
-      .then((data) => {
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' }
+    }
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e;
+    var r = /([^&;=]+)=?([^&;]*)/g;
+    var q = window.location.hash.substring(1);
+
+    e = r.exec(q)
+
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        console.log('response: ', response);
         this.setState({
-          users: data,
-          loaded: true
-        })
+          nowPlaying: {
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url
+          }
+        });
+      })
+  }
+
+  getArtistTopTracks() {
+    // Get an artist's top tracks
+    spotifyApi.getArtistTopTracks('0oSGxfWSnnOXhD2fKuz2Gy', 'GB')
+      .then(function (data) {
+        console.log(data.body);
+      }, function (err) {
+        console.log('Something went wrong!', err);
       });
-  };
+  }
 
   render() {
-    let users = this.state.users.map((item, i) => {
-      return <p>{item.name}</p>
-    });
-
     return (
-      <div>
-        {
-          this.state.loaded ? (
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-              </header>
-              {users}
-            </div>
-          ) : (
-              <div>
-                <p>L O A D I N G . . .</p>
-              </div>
-            )
+      <div className="App">
+        <a href='http://localhost:8888' > Login to Spotify </a>
+        <div>
+          Now Playing: {this.state.nowPlaying.name}
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+        </div>
+        {this.state.loggedIn &&
+          <button onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+        </button>
         }
       </div>
     );

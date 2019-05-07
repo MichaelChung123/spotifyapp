6e97ee3943cc13de91c26de0fda373ba17807105
MichaelChung123 <michael.chung1994@gmail.com>
2019-05-07 13:38:45 -0700

    showing top 10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SpotifyWebApi from 'spotify-web-api-js';
 
@@ -9,26 +8,23 @@ class App extends Component {
   constructor() {
     super();
     const params = this.getHashParams();
-    // console.log('Params: ', params);
     const token = params.access_token;
     if (token) {
       spotifyApi.setAccessToken(token);
     }
-
     this.state = {
       loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
+      nowPlaying: { name: 'Not Checked', albumArt: '', artistId: '' },
+      top10: null,
+      loaded: false
     }
   }
 
   getHashParams() {
     var hashParams = {};
-    var e;
-    var r = /([^&;=]+)=?([^&;]*)/g;
-    var q = window.location.hash.substring(1);
-
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
     e = r.exec(q)
-
     while (e) {
       hashParams[e[1]] = decodeURIComponent(e[2]);
       e = r.exec(q);
@@ -36,30 +32,43 @@ class App extends Component {
     return hashParams;
   }
 
-  getNowPlaying() {
+  getNowPlaying = () => {
     spotifyApi.getMyCurrentPlaybackState()
       .then((response) => {
-        console.log('response: ', response);
+        console.log("Response: ", response);
+        // console.log('id', response.item.artists[0].id);
         this.setState({
           nowPlaying: {
             name: response.item.name,
-            albumArt: response.item.album.images[0].url
+            albumArt: response.item.album.images[0].url,
+            artistId: response.item.artists[0].id
           }
         });
       })
   }
 
-  getArtistTopTracks() {
-    // Get an artist's top tracks
-    spotifyApi.getArtistTopTracks('0oSGxfWSnnOXhD2fKuz2Gy', 'GB')
-      .then(function (data) {
-        console.log(data.body);
+  getTopTrack = () => {
+    spotifyApi.getArtistTopTracks(this.state.nowPlaying.artistId, 'GB')
+      .then((response) => {
+        this.setState({
+          top10: response.tracks,
+          loaded: true
+        });
       }, function (err) {
         console.log('Something went wrong!', err);
       });
   }
 
   render() {
+    let top10 = '';
+
+    if (this.state.loaded) {
+      top10 = this.state.top10.map((item, i) => {
+        return <p key={i}>{item.name}</p>
+      });
+    }
+
+
     return (
       <div className="App">
         <a href='http://localhost:8888' > Login to Spotify </a>
@@ -69,10 +78,16 @@ class App extends Component {
         <div>
           <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
         </div>
+        <div>
+          {top10}
+        </div>
         {this.state.loggedIn &&
-          <button onClick={() => this.getNowPlaying()}>
+          <button onClick={this.getNowPlaying}>
             Check Now Playing
-        </button>
+            <button onClick={this.getTopTrack}>
+              List Top Tracks
+           </button>
+          </button>
         }
       </div>
     );
